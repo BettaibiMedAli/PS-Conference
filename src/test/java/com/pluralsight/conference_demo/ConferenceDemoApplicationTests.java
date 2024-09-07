77ba0425c5d0a87c849bec572de57f3db1daab80
BettaibiMedAli <mohamedalibettaibi22@gmail.com>
2024-09-07 13:24:33 +0200

    Initial commit for Spring Boot app

---
+++ src/test/java/com/pluralsight/conference_demo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conference_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
